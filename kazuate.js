fe2a84ae3073c750df87b5e98e16c4d75db35617
G384572023 <238457@st.takushoku-u.ac.jp>
2024-06-13 02:58:24 +0900

    完成

--- kazuate.js
+++ kazuate.js
@@ -2,13 +2,13 @@ let kotae = Math.floor(Math.random() * 10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 
 let kaisu = 0;
-let gameEnded = false; // ゲーム終了フラグ
+let gameEnded = false; 
 
 let a = document.querySelector('#kaito');
 a.addEventListener('click', hantei);
 
 function hantei() {
-  // ゲームが終了している場合のメッセージ
+  
   if (gameEnded) {
     let results = document.querySelector('p#result');
     results.textContent = "答えは " + kotae + " でした。すでにゲームは終わっています。";
@@ -26,14 +26,13 @@ function hantei() {
 
   let results = document.querySelector('p#result');
 
-  // 正解の場合
   if (yoso === kotae) {
     results.textContent = "正解です。おめでとう！";
-    gameEnded = true; // ゲーム終了フラグを設定
+    gameEnded = true;
   } else {
     if (kaisu === 3) {
       results.textContent = "まちがい。残念でした。答えは " + kotae + " です。";
-      gameEnded = true; // ゲーム終了フラグを設定
+      gameEnded = true;
     } else if (yoso < kotae) {
       results.textContent = "まちがい。答えはもっと大きいですよ。";
     } else {
--- weather.js
+++ weather.js
@@ -47,3 +47,95 @@ let data = {
 
 ////////// 課題3-2 ここからプログラムを書こう
 
+
+/*resultDiv.insertAdjacentHTML('beforeend', `<p>都市名 (City Name): ${data.name}</p>`);
+resultDiv.insertAdjacentHTML('beforeend', `<p>天気 (Weather Description): ${data.weather[0].description}</p>`);
+resultDiv.insertAdjacentHTML('beforeend', `<p>最低気温 (Minimum Temperature): ${data.main.temp_min}°C</p>`);
+resultDiv.insertAdjacentHTML('beforeend', `<p>最高気温 (Maximum Temperature): ${data.main.temp_max}°C</p>`);
+resultDiv.insertAdjacentHTML('beforeend', `<p>湿度 (Humidity): ${data.main.humidity}%</p>`);
+resultDiv.insertAdjacentHTML('beforeend', `<p>風速 (Wind Speed): ${data.wind.speed} m/s</p>`);
+resultDiv.insertAdjacentHTML('beforeend', `<p>風向 (Wind Direction): ${data.wind.deg} degrees</p>`);*/
+
+let selectBox = document.querySelector('select[name="selectBox"]');
+let btn = document.querySelector('button#btn');
+btn.addEventListener('click', sendRequest);
+
+// 通信を開始する処理
+function sendRequest() {
+  // URL を設定
+  let CityId = selectBox.value;
+
+  let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/' + CityId + '.json';
+
+  // 通信開始
+  axios.get(url)
+      .then(showResult)   // 通信成功
+      .catch(showError)   // 通信失敗
+      .then(finish);      // 通信の最後の処理
+}
+
+// 通信が成功した時の処理
+function showResult(resp) {
+    // サーバから送られてきたデータを出力
+    let data = resp.data;
+
+    // data が文字列型なら，オブジェクトに変換する
+    if (typeof data === 'string') {
+        data = JSON.parse(data);
+    }
+
+    // data をコンソールに出力
+    console.log(data);
+
+    // data.x を出力
+    console.log('都市名 (City Name):', data.name);
+    console.log('最低気温 (Minimum Temperature):', data.main.temp_min, '°C');
+    console.log('最高気温 (Maximum Temperature):', data.main.temp_max, '°C');
+    console.log('湿度 (Humidity):', data.main.humidity, '%');
+    console.log('風速 (Wind Speed):', data.wind.speed, 'm/s');
+    console.log('風向 (Wind Direction):', data.wind.deg, 'degrees');
+
+    // 新しいli要素を生成して、各データを表示
+    let cityName = document.createElement('p');
+    cityName.textContent = '都市名: ' + data.name;
+
+    let maxTemp = document.createElement('p');
+    maxTemp.textContent = '最高気温: ' + data.main.temp_max + '°C';
+
+    let minTemp = document.createElement('p');
+    minTemp.textContent = '最低気温: ' + data.main.temp_min + '°C';
+
+    let Humidity = document.createElement('p');
+    Humidity.textContent = '湿度: ' + data.main.humidity + '%';
+
+    let windSpeed = document.createElement('p');
+    windSpeed.textContent = '風速: ' + data.wind.speed + 'm/s';
+
+    let windDeg = document.createElement('p');
+    windDeg.textContent = '風向: ' + data.wind.deg + '°';
+
+    // 結果を表示するul要素を取得し、子要素として追加
+    let resultList = document.querySelector('#result');
+    
+    let resultLi = document.querySelectorAll('div#result > p');
+    for (let li of resultLi) {
+    li.remove();
+}
+
+    resultList.insertAdjacentElement('beforeend', cityName);
+    resultList.insertAdjacentElement('beforeend', maxTemp);
+    resultList.insertAdjacentElement('beforeend', minTemp);
+    resultList.insertAdjacentElement('beforeend', Humidity);
+    resultList.insertAdjacentElement('beforeend', windSpeed);
+    resultList.insertAdjacentElement('beforeend', windDeg);
+}
+
+// 通信エラーが発生した時の処理
+function showError(err) {
+    console.log(err);
+}
+
+// 通信の最後にいつも実行する処理
+function finish() {
+    console.log('Ajax 通信が終わりました');
+}
